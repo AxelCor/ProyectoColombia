7f0495edc2817fb5d5bdcb731cf661700d232a70
Axel Cordoba <axeelcordoba@gmail.com>
2023-05-02 19:39:22 -0300

    Cambiando v-Cel 2.0

---
+++ paginas/cuento.js
@@ -0,0 +1,15 @@
+var botonMenu = document.getElementById("boton-menu");
+botonMenu.addEventListener("click", function(e) {
+e.stopPropagation();
+document.querySelector(".dropdown").classList.toggle("show");
+});
+
+window.addEventListener("click", function() {
+var dropdowns = document.getElementsByClassName("dropdown");
+for (var i = 0; i < dropdowns.length; i++) {
+    var openDropdown = dropdowns[i];
+    if (openDropdown.classList.contains("show")) {
+    openDropdown.classList.remove("show");
+    };
+};
+});
