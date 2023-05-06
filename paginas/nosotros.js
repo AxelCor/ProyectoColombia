69997489ff150971cf61d549039b052e2d1acb11
Axel Cordoba <axeelcordoba@gmail.com>
2023-05-06 02:59:12 -0300

    Cambiando v-Cel Nosotros 3.0

--- paginas/nosotros.js
+++ paginas/nosotros.js
@@ -1,24 +1,51 @@
-const slides = document.querySelectorAll(".carousel-slide");
-const prevBtn = document.querySelector(".carousel-prev");
-const nextBtn = document.querySelector(".carousel-next");
-let slideIndex = 0;
+const slides = document.querySelectorAll('.carousel-slide');
+const prevBtn = document.querySelector('.carousel-prev');
+const nextBtn = document.querySelector('.carousel-next');
+let currentIndex = 0;
+let interval;
 
-showSlide(slideIndex);
-
-function showSlide(n) {
-  slides.forEach(slide => slide.style.transform = `translateX(-${n * 100}%)`);
+function startInterval() {
+  interval = setInterval(() => {
+    currentIndex++;
+    if (currentIndex >= slides.length) {
+      currentIndex = 0;
+    }
+    showSlide();
+  }, 6000);
 }
 
-function moveSlide(n) {
-  slideIndex += n;
-  if (slideIndex < 0) {
-    slideIndex = slides.length - 1;
-  } else if (slideIndex >= slides.length) {
-    slideIndex = 0;
+function stopInterval() {
+  clearInterval(interval);
+}
+
+function showSlide() {
+  slides.forEach((slide, index) => {
+    if (index === currentIndex) {
+      slide.classList.add('active');
+    } else {
+      slide.classList.remove('active');
+    }
+  });
+}
+
+prevBtn.addEventListener('click', () => {
+  currentIndex--;
+  if (currentIndex < 0) {
+    currentIndex = slides.length - 1;
   }
-  showSlide(slideIndex);
-}
+  stopInterval();
+  showSlide();
+  startInterval();
+});
 
-prevBtn.addEventListener("click", () => moveSlide(-1));
-nextBtn.addEventListener("click", () => moveSlide(1));
-console.log('El código JavaScript se está ejecutando');
+nextBtn.addEventListener('click', () => {
+  currentIndex++;
+  if (currentIndex >= slides.length) {
+    currentIndex = 0;
+  }
+  stopInterval();
+  showSlide();
+  startInterval();
+});
+
+startInterval();
