a8d2d800e90cb79c32bd9b3cc39cfbbaa9d34e36
Axel Cordoba <axeelcordoba@gmail.com>
2023-05-06 03:59:31 -0300

    Cambiando v-Cel Nosotros 3.3.2

--- paginas/nosotros.js
+++ paginas/nosotros.js
@@ -53,6 +53,8 @@ startInterval();
 const btn1 = document.getElementById('btn1');
 const btn2 = document.getElementById('btn2');
 const btn3 = document.getElementById('btn3');
+const btn4 = document.getElementById('btn4');
+const btn5 = document.getElementById('btn5');
 const overlay = document.getElementById('overlay');
 const overlayImage = document.getElementById('overlay-image');
 const overlayText = document.getElementById('overlay-text');
@@ -77,6 +79,18 @@ btn3.addEventListener('click', () => {
   overlayText.textContent = 'Alba Alexandra Rubio Ramírez nació en Ibagué, Colombia. Estudió arte Dramático en la Universidad del Valle, en la ciudad de Cali. Escritora de guiones para cine, poemas,obras de teatro infantil y juvenil, canciones, al igual que himnos Institucionales. Docente de Danzas y Teatro por más de 20 años, tanto en la Secretaria de Educación del Distrito como en la Gobernación de Cundinamarca en los que dirigió diferentes grupos teatrales, una de las obras ganadoras en el año 2000 fue “Sueños en el lumbral del Silencio” realizada con estudiantes de la Institución Educativa República de Panamá, los cuales tenían limitación auditiva. Asistió con el apoyo de la ONU a la Habana Cuba con la ponencia sobre inclusión de este tipo de estudiantes en las aulas regulares. Participó del diplomado sobre Modificalidad Cognitiva y pensamiento divergente realizado por la cooperativa de educadores del magisterio CODEMA, visitando los colegios que aplican esta metodología con sus estudiantes en Alemania, Francia, Suiza, Italia, y España. Ha participado de varios talleres de Creación Literaria realizados por IDARTES y trabaja activamente para difundir sus obras a las nuevas generaciones.';
   overlay.style.display = 'flex';
 });
+btn4.addEventListener('click', () => {
+    overlayImage.src = '../imagenes/soluna.jpeg';
+    overlayImage.alt = 'Imagen 4';
+    overlayText.textContent = 'Soluna Aguilar Rubio nació en Bogotá D.C. Ha vivido rodeada de arte gracias a sus padres maestros/directores de arte dramático, así como a sus viajes por el mundo. Inició un programa de Licenciatura en Educación de Lenguas Modernas en la Universidad Distrital. También ha estado trabajando y aprendiendo con niños en diferentes países. Su primer viaje al extranjero fue a Estados Unidos como salva vidas del campamento para “YMCA”, luego a Londres y Europa, Alemania, España, Holanda, Moscú, Estambul y Sudamérica, etc. También ha enseñado en universidades, escuelas y centros de idiomas en Bogotá. Soluna está inmersa en el teatro, el canto, la danza, las artes visuales y plásticas, se casó y junto a su esposo han sido parte importante en la elaboración de esta segunda edición del libro E-learning; ”Colombia, mi hermoso país”.';
+    overlay.style.display = 'flex';
+  });
+  btn5.addEventListener('click', () => {
+    overlayImage.src = '../imagenes/falco.jpg';
+    overlayImage.alt = 'Imagen 5';
+    overlayText.textContent = 'Falco Aguilar Rubio es un ingeniero civil colombiano, con máster en Ing. Recursos hídricos. de la Universidad Católica de Leuven. Con más de 8 años de experiencia. Actualmente es el Director en el área de negocios para Reino Unido en la compañía Estal. Ha Vivido en Bélgica, Holanda y después de un año sabático por el sureste asiático, reside actualmente en Barcelona. Es un apasionado de los viajes, la economía y la sostenibilidad, así como del arte escrito y visual. Esposo afectuoso y padre de dos hermosos hijos.';
+    overlay.style.display = 'flex';
+  });
 
 overlay.addEventListener('click', () => {
     overlay.style.display = 'none';
