ae65916c20a1bc32cabad68c0e302e4ed610d67a
Axel Cordoba <axeelcordoba@gmail.com>
2023-05-28 20:03:25 -0300

    agregando sweeralert2

--- paginas/cuento.js
+++ paginas/cuento.js
@@ -1,16 +1,21 @@
+import Swal from 'sweetalert2';
+
+Swal.fire('¡Hola!', 'Este es un mensaje de alerta', 'info');
+
+
 var botonMenu = document.getElementById("boton-menu");
-botonMenu.addEventListener("click", function(e) {
-e.stopPropagation();
-document.querySelector(".dropdown").classList.toggle("show");
+botonMenu.addEventListener("click", function (e) {
+    e.stopPropagation();
+    document.querySelector(".dropdown").classList.toggle("show");
 });
 
-window.addEventListener("click", function() {
-var dropdowns = document.getElementsByClassName("dropdown");
-for (var i = 0; i < dropdowns.length; i++) {
-    var openDropdown = dropdowns[i];
-    if (openDropdown.classList.contains("show")) {
-    openDropdown.classList.remove("show");
+window.addEventListener("click", function () {
+    var dropdowns = document.getElementsByClassName("dropdown");
+    for (var i = 0; i < dropdowns.length; i++) {
+        var openDropdown = dropdowns[i];
+        if (openDropdown.classList.contains("show")) {
+            openDropdown.classList.remove("show");
+        };
     };
-};
 });
 
